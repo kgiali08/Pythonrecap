c63420e6ab700055a03994d5a6daaf6218d9fd4c
Katie Giali <katie_giali1@baylor.edu>
2024-01-23 13:03:07 -0600

    updated print statement

--- MyGraph.py
+++ MyGraph.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #fix this! the module is not found 3 step process 
 import numpy as np
 
 x = np.linspace(0, 20, 100)
@@ -6,4 +6,6 @@ plt.plot(x, np.sin(x))
 
 plt.show()
 
-print("Hi there!")
+print("Hello World")
+
+#4000 files came from virtual environment (mathplotlib -> the dependencies)
